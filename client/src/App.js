373e12492cc58ffdcd5096354d23efa772cea46c
Itay Golan <>
2018-06-19 11:46:29 -0700

    show current played and playlists

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,92 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotifyAPI = new SpotifyWebApi(); 
+
 class App extends Component {
+  constructor() {
+    super();
+    const params = this.getHashParams();
+    const token = params.access_token;
+    if (token) {
+      spotifyAPI.setAccessToken(token);
+    }
+    this.state = {
+      loggedIn: token ? true : false,
+      nowPlaying: { name: 'Not Checked', albumArt: ''},
+      playlists: []
+    }
+    //console.log(params);
+  }
+
+  getHashParams() {
+    const hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    e = r.exec(q)
+    while (e) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+      e = r.exec(q);
+    }
+    return hashParams;
+  }
+
+  getNowPlaying() {
+    spotifyAPI.getMyCurrentPlaybackState()
+      .then((res) => {
+        this.setState({ 
+          nowPlaying: { name: res.item.name, albumArt: res.item.album.images[0].url}
+        });
+      })
+  }
+
+  getAllPlaylists() {
+    spotifyAPI.getUserPlaylists()
+      .then((res) => {
+          res.items.forEach(album => {
+            this.setState({
+              playlists: [...this.state.playlists, album.name]
+            });
+          });
+        });
+    console.log(this.state.playlists);
+  }
+
   render() {
+
+
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <a href='http://localhost:8888'> Login to Spotify </a>
+        <div>
+          Now Playing: { this.state.nowPlaying.name }
+        </div>
+        <div>
+          <img src={this.state.nowPlaying.albumArt} style={{ height : 150 }} />
+        </div>
+        <div>
+          {this.state.loggedIn && 
+          <button onClick={() => this.getNowPlaying()} >
+            Check Now Playing
+          </button>}
+        </div>
+        <div>
+          {this.state.loggedIn &&
+            <button onClick={() => this.getAllPlaylists()} >
+              See Albums
+          </button>}
+        </div>
+        <div>
+          Albums: {
+            this.state.loggedIn &&
+            <ul>
+              { this.state.playlists.map(data => <li key={data}>{data}</li>) }
+            </ul>
+          }
+        </div>
       </div>
     );
   }
